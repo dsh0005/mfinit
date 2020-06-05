df6c96f0d25c668a5c9c20c3534542877f5e4c18
Douglas Storm Hill <stormhill117@gmail.com>
2020-06-05 16:22:03 -0500

    Fix tabs/spaces. Ugh.

--- init.c
+++ init.c
@@ -13,8 +13,8 @@ int main(void)
 	if (sigfillset(&set)) return 2;
 	if (sigprocmask(SIG_BLOCK, &set, 0)) return 3;
 
-  status = fork();
-  if (status == -1) return 4;
+	status = fork();
+	if (status == -1) return 4;
 	else if (status) for (;;) wait(&status);
 
 	if (sigprocmask(SIG_UNBLOCK, &set, 0)) return 5;
@@ -22,5 +22,5 @@ int main(void)
 	if (setsid() == (pid_t)-1) return 6;
 	if (setpgid(0, 0)) return 7;
 	(void)execve("/etc/rc", (char * const []){ "rc", 0 }, (char * const[]){ 0 });
-  return 8;
+	return 8;
 }
